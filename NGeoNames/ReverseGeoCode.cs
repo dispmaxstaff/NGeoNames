1f03943b9f32f259b5bd32a6b22d414faffebe61
max <max@cosource.com.au>
2016-02-26 17:02:16 +0500

    fixed DoubleMath double registering

--- NGeoNames/ReverseGeoCode.cs
+++ NGeoNames/ReverseGeoCode.cs
@@ -204,27 +204,28 @@ namespace NGeoNames
             };
         }
 
-        /// <summary>
-        /// This class is for internal use (Kd-Tree) only.
-        /// </summary>
-        private class DoubleMath : KdTree.Math.TypeMath<double>
-        {
-            static DoubleMath()
-            {
-                // Make sure we register ourselves!
-                TypeMath<double>.Register(new DoubleMath());
-            }
-
-            public override double Add(double a, double b) { return a + b; }
-            public override bool AreEqual(double a, double b) { return a == b; }
-            public override int Compare(double a, double b) { return a.CompareTo(b); }
-            public override double MaxValue { get { return double.MaxValue; } }
-            public override double MinValue { get { return double.MinValue; } }
-            public override double Multiply(double a, double b) { return a * b; }
-            public override double NegativeInfinity { get { return double.NegativeInfinity; } }
-            public override double PositiveInfinity { get { return double.PositiveInfinity; } }
-            public override double Subtract(double a, double b) { return a - b; }
-            public override double Zero { get { return 0; } }
-        }
     }
+
+    /// <summary>
+    /// This class is for internal use (Kd-Tree) only.
+    /// </summary>
+    class DoubleMath : KdTree.Math.TypeMath<double>
+    {
+        static DoubleMath()
+        {
+            TypeMath<double>.Register(new DoubleMath());
+        }
+
+        public override double Add(double a, double b) { return a + b; }
+        public override bool AreEqual(double a, double b) { return a == b; }
+        public override int Compare(double a, double b) { return a.CompareTo(b); }
+        public override double MaxValue { get { return double.MaxValue; } }
+        public override double MinValue { get { return double.MinValue; } }
+        public override double Multiply(double a, double b) { return a * b; }
+        public override double NegativeInfinity { get { return double.NegativeInfinity; } }
+        public override double PositiveInfinity { get { return double.PositiveInfinity; } }
+        public override double Subtract(double a, double b) { return a - b; }
+        public override double Zero { get { return 0; } }
+    }
+
 }
